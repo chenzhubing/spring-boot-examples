1c80cc1f5ed882e1f48b5377cc2ac95323376a0f
chenzhubing <1939887953@qq.com>
2019-05-24 15:45:47 +0800

    commit...

---
+++ spring-boot-helloworld/src/main/java/com/czb/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.czb;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class HelloWorld {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
